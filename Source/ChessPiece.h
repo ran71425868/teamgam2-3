b1e82d6f1f0cf8fcf781fdb4254a41be0538a454
ran71425868 <2240182@ecc.ac.jp>
2025-11-27 11:37:38 +0900

    a

--- Source/ChessPiece.h
+++ Source/ChessPiece.h
@@ -31,7 +31,7 @@ public:
 
     virtual std::string getType() const = 0;
     virtual std::vector<Position> getLegalMoves(const Board& board, bool isForCheck = false) const=0;
-    //virtual std::shared_ptr<ChessPiece> clone() const = 0;
+    virtual std::shared_ptr<ChessPiece> clone() const = 0;
 
 
     void setPosition(Position p);
